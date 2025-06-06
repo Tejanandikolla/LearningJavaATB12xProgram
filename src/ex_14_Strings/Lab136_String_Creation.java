bd6ac139c2b86fe489a3bc2f390c119439338a87
Tejonandikolla <nandikollateja@gmail.com>
2025-06-07 00:25:58 +0530

    string

---
+++ src/ex_14_Strings/Lab136_String_Creation.java
@@ -0,0 +1,8 @@
+package ex_14_Strings;
+
+public class Lab136_String_Creation {
+    public static void main(String[] args) {
+        String s1 = "Teja";// SCP -> string constant pool
+        String s2 = new String ("Teja");// OA - Object area
+    }
+}
