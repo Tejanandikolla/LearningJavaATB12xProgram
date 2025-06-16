be1d42755c2630c1dfe69ddd4b132b002a29df81
Tejonandikolla <nandikollateja@gmail.com>
2025-06-16 19:07:41 +0530

    OOPS

---
+++ src/poly/methodoverriding/Father.java
@@ -0,0 +1,11 @@
+package poly.methodoverriding;
+
+public class Father {
+    void home(){
+        System.out.println("Father - 2BHK");
+    }
+
+    void f1(){
+        System.out.println("Father - f1");
+    }
+}
---
+++ src/poly/methodoverriding/Pramod.java
@@ -0,0 +1,13 @@
+package poly.methodoverriding;
+
+public class Pramod {
+    void p1(){
+        System.out.println("pramod - p1");
+    }
+
+//    @Override
+    void home(){
+        System.out.println("pramod - 3BHK");
+    }
+
+}
